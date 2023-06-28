31c2db805d8c42f638290489fad676db742355f4
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-28 07:58:05 -0600

    Added header logo and style

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <img src={logo} alt="logo" className="App-logo" />
       </header>
       hello
     </div>
